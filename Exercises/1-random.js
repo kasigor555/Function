f0d71da68e734bb6b9bafee08ec4469e3e563326
Igor Kas <kasigor555@gmail.com>
2019-11-17 00:26:15 +0300

    Выпослено задание 1

--- Exercises/1-random.js
+++ Exercises/1-random.js
@@ -1,9 +1,16 @@
 'use strict';
 
+// Generate random Number between from min to max
+// Use Math.random() and Math.floor()
+// See documentation at MDN
+// Конструкция (max - min + 1) + min) создаёт смещение
+// +1 нужно, чтобы в диапазон было включено число max
 const random = (min, max) => {
-  // Generate random Number between from min to max
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 module.exports = { random };
